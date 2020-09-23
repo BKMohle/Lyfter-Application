4e0b5307932b41d0d3e70a0ffebbeed6a825d302
BKMohle <brandonmohle42@gmail.com>
2020-09-23 15:29:13 -0500

    Can add multiple sets

--- src/components/RecordComponents/Sets/Sets.js
+++ src/components/RecordComponents/Sets/Sets.js
@@ -1,1 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import WorkingSet from './WorkingSet'
+
+const SetNumber = () => {
+
+   const [numberOfSets,setNumberOfSets] = useState(1)
+
+   let sets = []
+
+   function AddSet() {
+      setNumberOfSets(numberOfSets+1)
+   }
+
+   let i;
+   for (i = 0; i < numberOfSets; i++) {
+      sets.push(<WorkingSet/>)
+   }
+
+   return (
+      <>
+         Set
+         <div>
+            {sets}
+         </div>
+         <Button type="submit" onClick={AddSet}>+ Add Set</Button>
+      </>
+   )
+}
+
+export default SetNumber
