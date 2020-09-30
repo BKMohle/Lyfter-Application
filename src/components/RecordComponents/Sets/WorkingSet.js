d7d4233dd5fd3f92ec5f7416a953e923db1acc6c
BKMohle <brandonmohle42@gmail.com>
2020-09-30 13:37:44 -0500

    reps and weight are now individual components

--- src/components/RecordComponents/Sets/WorkingSet.js
+++ src/components/RecordComponents/Sets/WorkingSet.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+import Reps from '../Reps/Reps'
+import Weight from '../Weight/Weight'
 
 const WorkingSet = () => {
    
    return (
       <>
-      <div>
-         <Form>
-            <Form.Group controlId='formSetWeight'>
-               <Form.Label>lbs </Form.Label>
-               <Form.Control type="weight" />
-            </Form.Group>
-         </Form>
-      </div>
-      <div>
-         <Form>
-            <Form.Group controlId='formSetWeight'>
-               <Form.Label>Reps </Form.Label>
-               <Form.Control type="reps"/>
-            </Form.Group>
-         </Form>
-      </div>
+      <Reps/>
+      <Weight/>
       </>
    )
 
