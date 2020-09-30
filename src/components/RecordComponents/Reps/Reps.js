ed64dcc00ca8d780d1cdbdf06c9d2cae8f44761e
BKMohle <brandonmohle42@gmail.com>
2020-09-30 13:37:06 -0500

    Added reps logic / label

--- src/components/RecordComponents/Reps/Reps.js
+++ src/components/RecordComponents/Reps/Reps.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+
+const Reps = () => {
+   return (
+      <div>
+         <Form>
+            <Form.Group controlId='formSetWeight'>
+               <Form.Label>Reps </Form.Label>
+               <Form.Control type="reps"/>
+            </Form.Group>
+         </Form>
+      </div>
+   )
+}
+
+export default Reps
