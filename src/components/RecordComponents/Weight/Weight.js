867f607dc35d7d8e6fb1483eb9f0e9d3fec06091
BKMohle <brandonmohle42@gmail.com>
2020-09-30 13:37:23 -0500

    added weight logic / label

--- src/components/RecordComponents/Weight/Weight.js
+++ src/components/RecordComponents/Weight/Weight.js
@@ -1,2 +1,17 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
+
+const Weight = () => {
+   return (
+      <div>
+         <Form>
+            <Form.Group controlId='formSetWeight'>
+               <Form.Label>lbs </Form.Label>
+               <Form.Control type="weight" />
+            </Form.Group>
+         </Form>
+      </div>
+   )
+}
+
+export default Weight
