0c51f615323186da93266de32b31fde6e8f525b4
BKMohle <brandonmohle42@gmail.com>
2020-10-06 15:24:22 -0500

    added props to WorkingSet

--- src/components/RecordComponents/Sets/WorkingSet.js
+++ src/components/RecordComponents/Sets/WorkingSet.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Form from 'react-bootstrap/Form'
 import Reps from '../Reps/Reps'
 import Weight from '../Weight/Weight'
 
-const WorkingSet = () => {
-   
+const WorkingSet = (props) => {
+
    return (
       <>
+      {props.setNumber}
       <Reps/>
       <Weight/>
       </>
