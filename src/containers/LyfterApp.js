511999c6589137f72e7ab90cd4cbf46bc86a8692
BKMohle <brandonmohle42@gmail.com>
2020-10-06 15:24:37 -0500

    changed SetNumber component to Sets

--- src/containers/LyfterApp.js
+++ src/containers/LyfterApp.js
@@ -3,7 +3,7 @@ import Title from '../components/RecordComponents/Title/Title'
 import Container from 'react-bootstrap/Container'
 import SelectDate from '../components/RecordComponents/Date/Date'
 import ExerciseList from '../components/RecordComponents/Exercises/Exercises'
-import SetNumber from '../components/RecordComponents/Sets/Sets'
+import Sets from '../components/RecordComponents/Sets/Sets'
 
 const LyfterApp = (props) => {
     return (
@@ -11,7 +11,7 @@ const LyfterApp = (props) => {
         <Title />
         <SelectDate />
         <ExerciseList />
-        <SetNumber />
+        <Sets />
       </Container>
     )
   }
